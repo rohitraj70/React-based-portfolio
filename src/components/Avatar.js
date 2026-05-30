1c4b788e4c3705f15d455d641a63f549c360bdd6
Rohit Raj <rohit.tech0956@gmail.com>
2026-05-30 22:02:26 +0530

    Update Avatar component to use local image
    used my image

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,7 +3,7 @@
 function Avatar(){
     return(
         <div className="avatar-container">
-            <img src="https://images.unsplash.com/photo-1615109398623-88346a601842?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Profile" className="avtar-image" />  
+            <img src="/rohit.jpeg" alt="Profile" className="avtar-image" />  
             <div className="avatar-border"></div>  {/* decorative border around the avatar, can be styled with CSS*/}
         </div>
     )
